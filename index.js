9744a1139ef4ec051ddc7ab3ca406fc3c6aaf355
Philipp Busse <philipp.busse@gmx.net>
2017-10-26 07:59:59 +0200

    fix eslint errors

--- index.js
+++ index.js
@@ -1,138 +1,137 @@
-'use strict';
+'use strict'
 
-const cors = require('cors');
+const cors = require('cors')
 const debug = require('debug')('express-sharp')
-const etag = require('etag');
-const express = require('express');
-const http = require('http');
-const https = require('https');
-const sharp = require('sharp');
-const url = require('url');
-const expressValidator = require('express-validator');
+const etag = require('etag')
+const express = require('express')
+const http = require('http')
+const https = require('https')
+const sharp = require('sharp')
+const url = require('url')
+const expressValidator = require('express-validator')
 
 const transform = function(width, height, crop, gravity, cropMaxSize) {
-
-  const transformer = sharp();
+  const transformer = sharp()
   if (crop) {
-    const aspectRatio = width / height;
+    const aspectRatio = width / height
     if (width > cropMaxSize || height > cropMaxSize) {
       if (width > height) {
-        width = cropMaxSize;
-        height = Math.round(width / aspectRatio);
+        width = cropMaxSize
+        height = Math.round(width / aspectRatio)
       } else {
-        height = cropMaxSize;
-        width = Math.round(height * aspectRatio);
+        height = cropMaxSize
+        width = Math.round(height * aspectRatio)
       }
     }
-    transformer.resize(width, height).crop(gravity || sharp.gravity.center);
+    transformer.resize(width, height).crop(gravity || sharp.gravity.center)
   } else {
-    transformer.resize(width, height).min().withoutEnlargement();
+    transformer.resize(width, height).min().withoutEnlargement()
   }
-  return transformer;
-};
+  return transformer
+}
 
 const getImageUrl = function(baseHost, inputUrl) {
-  let imageUrl = url.parse(inputUrl);
-  imageUrl.host = baseHost.replace('https://', '').replace('http://', '');
-  imageUrl.protocol = baseHost.startsWith('https') ? 'https' : 'http';
-  return url.format(imageUrl);
-};
+  let imageUrl = url.parse(inputUrl)
+  imageUrl.host = baseHost.replace('https://', '').replace('http://', '')
+  imageUrl.protocol = baseHost.startsWith('https') ? 'https' : 'http'
+  return url.format(imageUrl)
+}
 
 module.exports = function(options) {
-  const router = express.Router();
+  const router = express.Router()
   router.use(expressValidator({
     customValidators: {
       isSharpFormat: function(value) {
-        return sharp.format.hasOwnProperty(value);
+        return sharp.format.hasOwnProperty(value)
       },
       isGravity: function(value) {
-        return sharp.gravity.hasOwnProperty(value);
+        return sharp.gravity.hasOwnProperty(value)
       },
       isQuality: function(value) {
-        return value >= 0 && value <= 100;
+        return value >= 0 && value <= 100
       },
       isUrlPathQuery: function(value) {
         if (!value) {
-          return false;
+          return false
         }
-        const u =  url.parse(value);
+        const u = url.parse(value)
         if (u.protocol || u.host || !u.path) {
-          return false;
+          return false
         }
-        return true;
+        return true
       },
     },
-  }));
+  }))
 
-  const _cors = cors(options.cors || {});
-  const cropMaxSize = options.cropMaxSize || 2000;
-  const protocol = (options.baseHost.startsWith('https')) ? https : http;
+  const _cors = cors(options.cors || {})
+  const cropMaxSize = options.cropMaxSize || 2000
+  const protocol = (options.baseHost.startsWith('https')) ? https : http
 
   router.get('/resize/:width/:height?', _cors, function(req, res, next) {
-    let format = req.query.format;
+    let format = req.query.format
     if (req.headers.accept && req.headers.accept.indexOf('image/webp') !== -1) {
-      format = format || 'webp';
+      format = format || 'webp'
     }
-    const quality = parseInt(req.query.quality || 75, 10);
+    const quality = parseInt(req.query.quality || 75, 10)
 
-    req.checkParams('height').optional().isInt();
-    req.checkParams('width').isInt();
-    req.checkQuery('format').optional().isSharpFormat();
-    req.checkQuery('quality').optional().isQuality();
-    req.checkQuery('progressive').optional().isBoolean();
-    req.checkQuery('crop').optional().isBoolean();
-    req.checkQuery('gravity').optional().isGravity();
-    req.checkQuery('url').isUrlPathQuery();
+    req.checkParams('height').optional().isInt()
+    req.checkParams('width').isInt()
+    req.checkQuery('format').optional().isSharpFormat()
+    req.checkQuery('quality').optional().isQuality()
+    req.checkQuery('progressive').optional().isBoolean()
+    req.checkQuery('crop').optional().isBoolean()
+    req.checkQuery('gravity').optional().isGravity()
+    req.checkQuery('url').isUrlPathQuery()
 
-    const errors = req.validationErrors();
+    const errors = req.validationErrors()
     if (errors) {
-      return res.status(400).json(errors);
+      return res.status(400).json(errors)
     }
 
-    const imageUrl = getImageUrl(options.baseHost, req.query.url);
+    const imageUrl = getImageUrl(options.baseHost, req.query.url)
 
-    const width = parseInt(req.params.width, 10);
-    const height = parseInt(req.params.height, 10) || null;
-    const crop = req.query.crop === 'true';
-    const gravity = req.query.gravity;
+    const width = parseInt(req.params.width, 10)
+    const height = parseInt(req.params.height, 10) || null
+    const crop = req.query.crop === 'true'
+    const gravity = req.query.gravity
     const transformer = transform(width, height, crop, gravity, cropMaxSize)
       .on('error', function sharpError(err) {
-        res.status(500);
-        next(new Error(err));
-      });
+        res.status(500)
+        next(new Error(err))
+      })
 
-    const etagBuffer = new Buffer([imageUrl, width, height, format, quality]);
-    res.setHeader('ETag', etag(etagBuffer, {weak: true}));
+    const etagBuffer = new Buffer([imageUrl, width, height, format, quality])
+    res.setHeader('ETag', etag(etagBuffer, {weak: true}))
     if (req.fresh) {
-      return res.sendStatus(304);
+      return res.sendStatus(304)
     }
 
-    debug('Requesting:', imageUrl);
+    debug('Requesting:', imageUrl)
 
     protocol
       .get(imageUrl, function getImage(result) {
-        debug('Requested %s. Status: %s', imageUrl, result.statusCode);
+        debug('Requested %s. Status: %s', imageUrl, result.statusCode)
         if (result.statusCode >= 400) {
-          return res.sendStatus(result.statusCode);
+          return res.sendStatus(result.statusCode)
         }
-        res.status(result.statusCode);
-        const inputFormat = result.headers['content-type'] || '';
-        format = format || inputFormat.replace('image/', '');
+        res.status(result.statusCode)
+        const inputFormat = result.headers['content-type'] || ''
+        format = format || inputFormat.replace('image/', '')
 
-        format = sharp.format.hasOwnProperty(format) ? format : 'jpeg';
+        format = sharp.format.hasOwnProperty(format) ? format : 'jpeg'
         transformer[format]({
           quality: quality,
           progressive: req.query.progressive === 'true',
-        });
+        })
 
-        res.type('image/' + format);
-        result.pipe(transformer).pipe(res);
+        res.type('image/' + format)
+        result.pipe(transformer).pipe(res)
       })
       .on('error', function(err) {
-        next(new Error(err));
-      });
-  });
+        next(new Error(err))
+      })
+  })
 
-  return router;
-};
-module.exports.getImageUrl = getImageUrl;
+  return router
+}
+module.exports.getImageUrl = getImageUrl
--- test/app.js
+++ test/app.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
-var express = require('express');
-var app = express();
-var scale = require('..');
+var express = require('express')
+var app = express()
+var scale = require('..')
 
-app.use('/scale', scale({baseHost: 'lorempixel.com'}));
+app.use('/scale', scale({baseHost: 'lorempixel.com'}))
 
 var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
+  var host = server.address().address
+  var port = server.address().port
 
-  console.log('✔ Example app listening at http://%s:%s', host, port);
-});
+  console.log('✔ Example app listening at http://%s:%s', host, port)
+})
