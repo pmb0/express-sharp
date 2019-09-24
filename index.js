400de82971a1de277212019afb6f2b3de94e3cb5
Dave Macaulay <macaulay@adobe.com>
2019-09-24 13:21:22 -0500

    Fix rejectUnauthorized check

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ module.exports = function(options) {
 
       debug('Requesting:', imageUrl)
       let response = await request({
-        rejectUnauthorized: !!process.env.NODE_TLS_REJECT_UNAUTHORIZED,
+        rejectUnauthorized: String(process.env.NODE_TLS_REJECT_UNAUTHORIZED) !== "0",
         encoding: null,
         uri: imageUrl,
         resolveWithFullResponse: true,
