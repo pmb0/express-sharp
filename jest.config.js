805449c77221760b2e723616798cb6b076658807
Philipp Busse <philipp.busse@gmx.net>
2020-06-08 00:43:45 +0200

    ci: provide lcov coverage report

--- jest.config.js
+++ jest.config.js
@@ -31,12 +31,7 @@ module.exports = {
   coveragePathIgnorePatterns: ['.dto.ts$'],
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: ['text', 'lcov'],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
