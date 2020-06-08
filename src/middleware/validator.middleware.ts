8414307c8a14c67babf97ddd029b65028039ec8c
Philipp Busse <philipp.busse@gmx.net>
2020-06-09 01:47:49 +0200

    chore: remove eslint-disable comment

--- src/middleware/validator.middleware.ts
+++ src/middleware/validator.middleware.ts
@@ -1,8 +1,8 @@
+import { plainToClass } from 'class-transformer'
+import { ClassType } from 'class-transformer/ClassTransformer'
+import { validate as validate_, ValidationError } from 'class-validator'
 import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { BadRequestException } from '../http-exception'
-import { plainToClass } from 'class-transformer'
-import { ValidationError, validate as validate_ } from 'class-validator'
-import { ClassType } from 'class-transformer/ClassTransformer'
 
 export function validate<T>(Dto: ClassType<T>): RequestHandler {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -24,8 +24,7 @@ export function validate<T>(Dto: ClassType<T>): RequestHandler {
           .join(', ')
         next(new BadRequestException(message))
       } else {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        res.locals.dto = dto
+        ;(res.locals as { dto: T }).dto = dto
         next()
       }
     } catch (error) {
