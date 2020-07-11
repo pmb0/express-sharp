5a15e14b1be2a8fe2825ef9213b6123ad08964ba
Philipp Busse <philipp.busse@gmx.net>
2020-07-11 14:40:59 +0200

    fix(exception): set the `stack` property on exception objects

--- src/http-exception.ts
+++ src/http-exception.ts
@@ -1,23 +1,27 @@
 export class HttpException extends Error {
   constructor(public readonly status: number, message: string) {
     super(message)
+    Error.captureStackTrace(this, HttpException)
   }
 }
 
 export class BadRequestException extends HttpException {
   constructor(message: string) {
     super(400, message)
+    Error.captureStackTrace(this, BadRequestException)
   }
 }
 
 export class NotFoundException extends HttpException {
   constructor(message: string) {
     super(404, message)
+    Error.captureStackTrace(this, NotFoundException)
   }
 }
 
 export class ForbiddenException extends HttpException {
   constructor(message: string) {
     super(403, message)
+    Error.captureStackTrace(this, ForbiddenException)
   }
 }
