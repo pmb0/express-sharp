927dde5dada9913579357ca03f620bffc96fcf08
Philipp Busse <philipp.busse@gmx.net>
2021-05-18 13:51:04 +0200

    chore: do not look for mocks in dist

--- jest.config.js
+++ jest.config.js
@@ -166,7 +166,7 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
-  // unmockedModulePathPatterns: undefined,
+  modulePathIgnorePatterns: ["<rootDir>/dist/"]
 
   // Indicates whether each individual test should be reported during the run
   // verbose: undefined,
